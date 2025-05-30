9986f799dbc37f455fed4d3ca0475cdf9d9afd3b
Serge <33496306+skozubek@users.noreply.github.com>
2025-05-30 20:44:35 +0200

    Fix Frame component import

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { Frame } from "./screens/Frame";
+import { Frame } from "./screens/Frame/Frame";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
