ace08560ad911dff381469562067db2c8571484a
Serge <33496306+skozubek@users.noreply.github.com>
2025-06-07 19:03:58 +0200

    Create Public User Profile Page

--- src/screens/Frame/Frame.tsx
+++ src/screens/Frame/Frame.tsx
@@ -14,6 +14,7 @@ import { ProjectDetail } from "../ProjectDetail";
 import { CreateStartSnap } from "../CreateStartSnap";
 import { EditStartSnap } from "../EditStartSnap";
 import { Profile } from "../Profile";
+import { PublicProfile } from "../PublicProfile";
 import { AuthProvider, useAuth } from "../../context/AuthContext";
 import { ScrollToTop } from "../../components/utils/ScrollToTop";
 
@@ -57,6 +58,7 @@ export const Frame = (): JSX.Element => {
             <Route path="/" element={<MainContentSection />} />
             <Route path="/projects" element={<Projects />} />
             <Route path="/projects/:slug" element={<ProjectDetail />} />
+            <Route path="/profiles/:username" element={<PublicProfile />} />
             <Route
               path="/create"
               element={
---
+++ src/screens/PublicProfile/index.ts
@@ -0,0 +1,6 @@
+/**
+ * src/screens/PublicProfile/index.ts
+ * @description Barrel file that exports the PublicProfile component
+ */
+
+export { PublicProfile } from "./PublicProfile";
